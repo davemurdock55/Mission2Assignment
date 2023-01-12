4fbd653853a8674e29cbfee8215f1798b3852b07
David Murdock <murdodav@byu.edu>
2023-01-12 16:44:34 -0700

    Got the dice rolling functionality to work and tested it.

---
+++ Mission2Assignment/Die.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2Assignment
+{
+    public class Die
+    {
+        public int RollDie()
+        {
+            // creating a new "Random" object
+            Random rnd = new Random();
+            // generates a + random int within the sepecified range (includes min, excludes max)
+            int numRolled = rnd.Next(1, 7);
+
+            return numRolled;
+        }
+    }
+}
--- Mission2Assignment/Program.cs
+++ Mission2Assignment/Program.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿// David Murdock
+// simulates rolling 2 6d and gives you a histogram of the percentage of each total rolled
+
+
+using System;
 
 namespace Mission2Assignment
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            // simulate rolling 2 6d
+
             // an array to keep track of the number of times each total number is thrown
-            short[] aRollNumTotals = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] aRollNumTotals = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            // Initializing 2 die objects of the Die class
+            Die dice1 = new Die();
+            Die dice2 = new Die();
+
+            int die1_result = dice1.RollDie();
+            int die2_result = dice2.RollDie();
+            
+
+            // allow the user to choose how many times the 2 dice will be thrown
+            // Once teh dice have been throw a certain number of times, print a histogram using the * character
+            // that shows the total percentage each number was rolled.
+            // each * will represent 1% of rolls
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            // Console.Write("How many dice rolls would you like to simulate?");
+
+            Console.WriteLine("Die 1: " + die1_result);
+            Console.WriteLine("Die 2: " + die2_result);
+
         }
     }
 }
