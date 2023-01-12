cefb741f8c154bf0db8bde688cace2e1f262b24e
davem <davem@DESKTOP-B2FUG72>
2023-01-12 16:08:30 -0700

    Add project files.

---
+++ Mission2Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mission2Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // simulate rolling 2 6d
+            // an array to keep track of the number of times each total number is thrown
+            // array aRollNumTotals
+        }
+    }
+}
+
+
