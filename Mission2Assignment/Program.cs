2afc2bf7659edb6c705fdbdc689d2f1bd67113a8
David Murdock <murdodav@byu.edu>
2023-01-12 17:00:15 -0700

    Asking the user how many times they would like to roll the dice

--- Mission2Assignment/Program.cs
+++ Mission2Assignment/Program.cs
@@ -8,10 +8,17 @@ namespace Mission2Assignment
 {
     class Program
     {
+        // Overall Instructions:
+        // allow the user to choose how many times the 2 dice will be thrown
+        // Once the dice have been throw a certain number of times, print a histogram using the * character
+        // that shows the total percentage each number was rolled.
+        // each * will represent 1% of rolls
+
         public static void Main(string[] args)
         {
             // initializing the die_total int variable
-            int die_total = 0;
+            int diceTotal = 0;
+            int iNumRolls = 0;
 
             // an array to keep track of the number of times each total number is thrown
             int[] aRollNumTotals = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -20,19 +27,25 @@ namespace Mission2Assignment
             Die die1 = new Die();
             Die die2 = new Die();
 
+
+
+            // Welcoming the User
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+
+            // Asking the user how many times they want to throw the two dice
+            Console.Write("How many dice rolls would you like to simulate?");
+
+            // getting the user input 
+            iNumRolls = int.Parse(Console.ReadLine());
+
             // rolling both dice and getting their results
-            int die1_result = die1.RollDie();
-            int die2_result = die2.RollDie();
+            int die1Result = die1.RollDie();
+            int die2Result = die2.RollDie();
 
             // the die_total is the die_total plus the sum of the rolled results
-            die_total += die1_result + die2_result;
+            diceTotal += die1Result + die2Result;
             
-            // allow the user to choose how many times the 2 dice will be thrown
-            // Once teh dice have been throw a certain number of times, print a histogram using the * character
-            // that shows the total percentage each number was rolled.
-            // each * will represent 1% of rolls
-            Console.WriteLine("Welcome to the dice throwing simulator!");
-            // Console.Write("How many dice rolls would you like to simulate?");
+            
 
             // Console.WriteLine("Die 1: " + die1_result);
             // Console.WriteLine("Die 2: " + die2_result);
